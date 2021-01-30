ae67f55f30d5a2a3701418efa80118488c1a7bea
Gajjar Mitul <gajjarmitul007@gmail.com>
2021-01-30 22:49:31 +0530

    20210130 -2249

--- resources/views/backend/exports/subscribers.blade.php
+++ resources/views/backend/exports/subscribers.blade.php
@@ -1,15 +1,15 @@
-<table style="margin-left: auto; margin-right: auto;">
+<table style="margin-left: auto; table-layout: fixed; width: 100%; margin-right: auto;">
     <tbody>
         @if(isset($data) && !empty($data))
             @foreach($data as $key => $val)
                 <tr style="height:300px; margin-top:5px">
                     @foreach($val as $row)
-                        <td style="width:30px; height:130px">
-                            <div><b>To, {{ $row->city_name ?? '' }}</b></div><br/><br/>
+                        <td style="width:27px; height:130px ; word-wrap: break-word ;padding-right: 2px;padding-left: 2px">
+                            <div style="margin-bottom:20px"><b>To, {{ $row->city_name ?? '' }}</b></div><br/>
                             <div><b>{{ $row->firstname ?? '' }} {{ $row->lastname ?? '' }}</b></div><br/>
                             <div><b>{{ $row->address ?? '' }}</b></div><br/>
                             <div><b>{{ $row->city_name ?? '' }} - {{ $row->pincode ?? '' }}</b></div><br/>
-                            <div><b>{{ $row->taluka_name ?? '' }} - {{ $row->district_name ?? '' }}</b></div><br/><br/>
+                            <div><b>{{ $row->taluka_name ?? '' }} - {{ $row->district_name ?? '' }}</b></div><br/>
                         </td>
                     @endforeach
                 </tr>
