8329baf05d6948a2c4e47038ac3c4433c74b81bd
Gajjar Mitul <gajjarmitul007@gmail.com>
2021-02-09 16:02:16 +0530

    20210209 -1602

--- resources/views/backend/exports/subscribers.blade.php
+++ resources/views/backend/exports/subscribers.blade.php
@@ -2,9 +2,9 @@
     <tbody>
         @if(isset($data) && !empty($data))
             @foreach($data as $key => $val)
-                <tr style="height:300px; margin-top:5px">
+                <tr style="height:350px; margin-top:5px">
                     @foreach($val as $row)
-                        <td style="width:27px; height:130px ; word-wrap: break-word ;padding-right: 2px;padding-left: 2px">
+                        <td style="width:27px; height:150px ; word-wrap: break-word ;padding-right: 2px;padding-left: 2px">
                             <div style="margin-bottom:20px"><b>To, {{ strtoupper($row->city_name) ?? '' }}</b></div><br/>
                             <div><b>{{ strtoupper($row->firstname) ?? '' }} {{ strtoupper($row->lastname) ?? '' }}</b></div><br/>
                             <div><b>{{ strtoupper($row->address) ?? '' }}</b></div><br/>
